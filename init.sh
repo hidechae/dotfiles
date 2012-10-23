81e61b34c1d15fe52e06ad46aa8a2eba30cf33f0
root <root@dti-vps-srv120.(none)>
2012-10-24 04:04:59 +0900

    add simlink

--- init.sh
+++ init.sh
@@ -10,3 +10,10 @@ echo 'source $HOME/rc/.zshrc
 source $HOME/rc/.screenrc' > $HOME/.zshrc
 
 source $HOME/.zshrc
+
+parent="./$0"
+parent="${parent%/*}"
+ln -s -i "$parent/.vimrc" "$home/.vimrc"
+ln -s -i "$parent/.vim" "$home/.vim"
+ln -s -i "$parent/.zshrc" "$home/.zshrc"
+ln -s -i "$parent/.screenrc" "$home/.screenrc"
