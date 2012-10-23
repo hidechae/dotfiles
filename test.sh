b54ca15a3c7a9014c0bf7f9f9da9ae9769e86b6b
hideyuki-utsunomiya <hidechae@debian.localhost>
2012-10-24 05:18:28 +0900

    delete test.sh again

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-parent="./$0"
-parent="${parent%/*}"
-ln -s -i "$parent/.vimrc" "$home/.vimrc"
-ln -s -i "$parent/.vim" "$home/.vim"
-ln -s -i "$parent/.zshrc" "$home/.zshrc"
-ln -s -i "$parent/.screenrc" "$home/.screenrc"
