75842012b15dea021f710220361e9403e7f91a66
Hideyuki Utsunomiya <hideyuki.utsunomiya@gree.net>
2014-05-20 20:37:51 +0900

    add .bashrc

--- init.sh
+++ init.sh
@@ -24,8 +24,10 @@ if [ -d "$HOME/bin" ]; then
 fi
 sudo ln -s -i "$parent/bin" "$HOME/bin"
 sudo ln -s -i "$parent/.vimrc" "$HOME/.vimrc"
-sudo ln -s -i "$parent/.vimshrc" "$HOME/.vimshrc"
-sudo ln -s -i "$parent/.zshrc" "$HOME/.zshrc"
+# sudo ln -s -i "$parent/.zshrc" "$HOME/.zshrc"
+sudo ln -s -i "$parent/.bashrc" "$HOME/.bashrc"
+sudo ln -s -i "$parent/.bash_profile" "$HOME/.bash_profile"
+sudo ln -s -i "$parent/.inputrc" "$HOME/.inputrc"
 sudo ln -s -i "$parent/.screenrc" "$HOME/.screenrc"
 sudo ln -s -i "$parent/.gitconfig" "$HOME/.gitconfig"
 sudo ln -s -i "$parent/.my.cnf" "$HOME/.my.cnf"
